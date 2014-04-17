e5e61f48fc5b408110e697d3610d39530ad6fd5a
rewuxiin <rewuxiin@gmail.com>
2014-04-17 23:31:28 +0300

    Test push.

--- module/Application/src/Application/Controller/SalaryController.php
+++ module/Application/src/Application/Controller/SalaryController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Application\Controller;
 
+use Application\Entity\Position;
+use Application\Entity\Vacancy;
+use Application\Entity\Technology;
+use Zend;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
@@ -8,7 +12,12 @@ class SalaryController extends AbstractActionController
 {
     public function indexAction()
     {
-        printf("Salary index");
+        $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');        
+        
+        
+        $getTechnology = $objectManager->find('Application\Entity\Technology', 1);
+        
+        printf($getTechnology->getName());
         return new ViewModel();
     }
 }
