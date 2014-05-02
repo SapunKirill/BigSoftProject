4a3366d94b645f82c8b5567bd8b799163673a4fa
rewuxiin <rewuxiin@gmail.com>
2014-05-02 22:37:04 +0300

    changed SearchAction now looking for options

--- module/Application/src/Application/Controller/SalaryController.php
+++ module/Application/src/Application/Controller/SalaryController.php
@@ -38,13 +38,13 @@ class SalaryController extends AbstractActionController {
     }
 
     public function searchAction() {
-        $position_id = $this->params('position');
-        $technology_id = $this->params('technology');
-        $city_id = $this->params('city');
-        $experience = $this->params('experience');
+        $position_id = $this->params()->fromQuery("position");
+        $technology_id = $this->params()->fromQuery("technology");
+        $city_id = $this->params()->fromQuery("city");
+        $experience = $this->params()->fromQuery("experience");
         $price = 0;
         $needs = array();
-
+        $experienceMin = 0;
         $validator = new Zend\Validator\Digits();
 
         if ($validator->isValid($position_id) && strlen($position_id) <= 4 && $position_id != 0) {
@@ -57,43 +57,51 @@ class SalaryController extends AbstractActionController {
             $needs['city_id'] = $city_id;
         }
         if ($validator->isValid($experience) && strlen($experience) <= 2 && $experience != 0) {
-            $needs['experience'] = $experience;
+            $experienceMin = $experience;
         }
-        if ($validator->isValid($this->params('price')) && strlen($this->params('price')) <= 6) {
-            $price = $this->params('price');
+        if ($validator->isValid($this->params()->fromQuery("price")) && strlen($this->params()->fromQuery("price")) <= 6) {
+            $price = $this->params()->fromQuery("price");
         }
 
-        $needs_id = 0;
         $resultNeeds = $this->getEntityManager()
                 ->getRepository('Application\Entity\Needs')
                 ->findBy($needs);
-        foreach ($resultNeeds as $value) {
-            $needs_id = $value->getId();
-        }
-        $vacancyId = 0;
-        $resultVacancyNeeds = $this->getEntityManager()
-                ->getRepository('Application\Entity\VacancyNeeds')
-                ->findBy(array('needs' => $needs_id));
-        foreach ($resultVacancyNeeds as $value) {
-            $vacancyId = $value->getVacancyId();
+
+        $needsId = array();
+        if ($experienceMin !== 0) {
+            foreach ($resultNeeds as $value) {
+                if ($value->getExperience() <= $experienceMin) {
+                    $needsId[] = $value->getId();
+                }
+            }
+        } else {
+            foreach ($resultNeeds as $value) {
+                $needsId[] = $value->getId();
+            }
         }
 
-        if ($price != 0) {
-            return new ViewModel(array(
-                'vacancy' => $this->getEntityManager()
-                        ->getRepository('Application\Entity\Vacancy')
-                        ->findBy(array('id' => $vacancyId,
-                            'price' => $price,
-                        )),
-            ));
+        $vacancyNeedsId = $this->getEntityManager()
+                ->getRepository('Application\Entity\VacancyNeeds')
+                ->findBy(array('needs' => $needsId));
+
+
+        $vacancy_id = array();
+        if ($price !== 0) {
+            foreach ($vacancyNeedsId as $value) {
+                if ($value->getVacancy()->getPrice() >= $price) {
+                    $vacancy_id[] = $value->getVacancy()->getId();
+                }
+            }
         } else {
-            return new ViewModel(array(
-                'vacancy' => $this->getEntityManager()
-                        ->getRepository('Application\Entity\Vacancy')
-                        ->findBy(array('id' => $vacancyId,
-                        )),
-            ));
+            foreach ($vacancyNeedsId as $value) {
+                $vacancy_id[] = $value->getVacancy()->getId();
+            }
         }
+
+        return new ViewModel(array(
+            'vacancy' => $this->getEntityManager()
+                    ->getRepository('Application\Entity\Vacancy')
+                    ->findById($vacancy_id)));
     }
 
 }
--- module/Application/src/Application/Entity/Vacancy.php
+++ module/Application/src/Application/Entity/Vacancy.php
@@ -2,8 +2,9 @@
 namespace Application\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
-
-/** @ORM\Entity */
+/**
+ * @ORM\Entity(repositoryClass="Application\Repository\VacancyRepository")
+ */
 class Vacancy {
     /**
      * @ORM\Id
